e4afdde3a50fb40711d17c4a60b392b3ea6d283e
ponmalar207 <41136875+ponmalar207@users.noreply.github.com>
2018-07-13 14:40:33 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char c='a';
+		if(c=='a'||c=='e'||c=='o'||c=='i'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+			System.out.println("vowel");
+		}
+		else{
+			System.out.println("consonant");
+		}
+	}
