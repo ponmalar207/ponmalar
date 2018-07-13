6a064ee3ab620b1aad819687f9790b50392b61f9
ponmalar207 <41136875+ponmalar207@users.noreply.github.com>
2018-07-13 19:14:32 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s="Hello";
+		int n=2;
+		for(int i=0;i<n;i++){
+			System.out.println(s);
+		}
+	}
+}
