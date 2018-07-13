fc595fe83f7c9d02e05ebe80adcdf1c8ed7f6609
ponmalar207 <41136875+ponmalar207@users.noreply.github.com>
2018-07-13 13:49:57 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,16 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int c=3;
+		if(c>0){
+			System.out.println("positive");
+		}
+		else if(c<0){
+			System.out.println("negative");
+		}
+		else{
+			System.out.println("zero");
+		}
+	}
+}
