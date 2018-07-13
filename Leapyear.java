8ed7602593a785c1d987b9cd4cb6cc3f6c62d24b
ponmalar207 <41136875+ponmalar207@users.noreply.github.com>
2018-07-13 18:08:16 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=1999;
+		if(a%4==0){
+			System.out.println("LeapYear");
+		}
+		else{
+			System.out.println("not leap year");
+		}
+	}
+}
